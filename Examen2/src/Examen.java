d3e4fa6920560f63bb49f53254a704dd967de150
DiegoVegaProgra <90019265+JuanDiego21ing@users.noreply.github.com>
2023-03-25 20:05:03 -0600

    Update Examen.java

--- Examen2/src/Examen.java
+++ Examen2/src/Examen.java
@@ -1,5 +1,5 @@
 //DISEÑADO Y PROGRAMADO POR JUAN DIEGO VEGA ROMERO
-// PRUEBA COMMIT
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -8,8 +8,12 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class Examen extends JFrame {
@@ -26,6 +30,35 @@ public class Examen extends JFrame {
 			this.setLocationRelativeTo(null);
 			this.getContentPane().setBackground(Color.decode("#A8C6E6"));
 			
+			//MENU BAR
+			JMenuBar jmb = new JMenuBar();
+			jmb.setSize(this.getWidth(),40);
+			jmb.setVisible(true);
+			this.add(jmb);
+			 
+			JMenu jm1 = new JMenu("Cuenta");
+			JMenu jm2 = new JMenu("Usuarios");
+			JMenu jm3 = new JMenu("Ayuda");
+			 
+			jmb.add(jm1);
+			jmb.add(jm2);
+			jmb.add(jm3);
+			
+			JMenuItem jmi1 = new JMenuItem("Mi Cuenta");
+			JMenuItem jmi2 = new JMenuItem("Cerrar Sesion");
+			 
+			JMenuItem jmi3 = new JMenuItem("Lista de Usuarios");
+			JMenuItem jmi4 = new JMenuItem("Crear Usuario");
+			
+			JMenuItem jmi5 = new JMenuItem("¿Como Crear Usuarios?");
+			
+			jm1.add(jmi1);
+			jm1.add(jmi2);
+			
+			jm2.add(jmi3);
+			jm2.add(jmi4);
+			
+			jm3.add(jmi5);
 			
 			//PANEL ACCEDER CUENTA
 			
@@ -78,6 +111,39 @@ public class Examen extends JFrame {
 			boton2.setLocation(120,360);
 			boton2.setBackground(Color.decode("#E6A9A8"));
 			Acceder.add(boton2);
+			
+			//PANEL ¿COMO CREAR USUARIOS?
+			JPanel Crear = new JPanel();
+			Crear.setVisible(true);
+			Crear.setSize(500,600);
+			Crear.setBackground(Color.decode("#A8C6E6"));
+			Crear.setLayout(null);
+			add(Crear);
+			
+		    	JTextArea pasos = new JTextArea();
+		    	pasos.setSize(100,100);
+		    	pasos.setLocation(50,100);
+		    	pasos.setText("hola");
+		    	Crear.add(pasos);
+			
+			JLabel tag3 = new JLabel ("¿Como Crear Usuarios?");
+			tag3.setSize(300,30);
+			tag3.setLocation(115,70);
+			tag3.setFont(new Font("Arial",Font.BOLD,22));
+			Crear.add(tag3);
+			
+			
+			 
+			 jmi5.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					add(Crear);
+					remove(Acceder);
+					repaint();
+					
+				}});
 			
 			boton2.addActionListener(new ActionListener() {
 
